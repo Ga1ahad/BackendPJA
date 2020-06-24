d5242d3f341f699d9db7dbdd3e0e9e8d8b469fc9
Adam Nguyen <adanguye@publicisgroupe.net>
2020-06-24 14:29:33 +0200

    Przygotowanie końcówki do rejestracji użytkowników

---
+++ BackendApp/Controllers/RegisterController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BackendApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendApp.Controllers
+{
+    [Route("api/users/{IdUser:int}/[controller]")]
+    [ApiController]
+    public class RegisterController : Controller
+    {
+        private readonly s15264Context _context;
+        public RegisterController(s15264Context context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult CreateUser(User user)
+        {
+            _context.User.Add(user);
+            _context.SaveChanges();
+            return StatusCode(201, user);
+        }
+
+    }
+}
