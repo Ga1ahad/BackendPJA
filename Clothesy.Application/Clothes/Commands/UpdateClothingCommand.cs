fc29cdbad50b70fed1f8d5b4f2835524d31b3f39
Adam Nguyen <adanguye@publicisgroupe.net>
2021-07-29 18:05:24 +0200

    cloth edit

--- Clothesy.Application/Clothes/Commands/UpdateClothingCommand.cs
+++ Clothesy.Application/Clothes/Commands/UpdateClothingCommand.cs
@@ -1,41 +1,56 @@
 ﻿using Clothesy.Application.Persistence;
+using Clothesy.Domain.Entities;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Clothesy.Application.Clothes.Commands
 {
-    public class UpdateClothingCommand : IRequest<int>
+    public class UpdateClothesCommand : IRequest<int>
     {
-        public int idClothing { get; set; }
 
         public string Name { get; set; }
+        public string Url { get; set; }
+        public int idClothingType { get; set; }
+        public int idUser { get; set; }
+        public string Tags { get; set; }
 
-        public class UpdateClothingCommandHandler : IRequestHandler<UpdateClothingCommand, int>
+        public class UpdateClothesCommandHandler : IRequestHandler<UpdateClothesCommand, int>
         {
             private readonly IClothesyDb _context;
-            public UpdateClothingCommandHandler(IClothesyDb context)
+            public UpdateClothesCommandHandler(IClothesyDb context)
             {
                 _context = context;
             }
 
-            public async Task<int> Handle(UpdateClothingCommand request, CancellationToken cancellationToken)
+            public async Task<int> Handle(UpdateClothesCommand request, CancellationToken cancellationToken)
             {
-                var cloting = await _context.Clothing.Where(t => t.idClothing == request.idClothing).FirstOrDefaultAsync(cancellationToken: cancellationToken);
-
-                if (cloting == null)
+                //var clothing = new Clothing();
+                var clothing = _context.Clothing.Single(a => a.idClothing == 46);
+                var old_tags = _context.ClothingTag.Where(c => c.idClothing == 46);
+                clothing.Name = request.Name;
+                clothing.idClothingType = request.idClothingType;
+                string tagReq = request.Tags;
+                string[] tags = tagReq.Split(',');
+                foreach (var book in old_tags)
                 {
-                    return default;
+                    clothing.ClothingTag.Remove(book);
                 }
-                else
+                //    var toRemoveModels = _context.ClothingTag
+                //                          .Where(c => c.idClothing == request.idClothing)
+                //                           .ToList();
+                //  clothing.ClothingTag.Clear(new ClothingTag { idClothing = clothing.idClothing });
+                foreach (var tag in tags)
                 {
-                    cloting.Name = request.Name;
-                    await _context.SaveChangesAsync(cancellationToken);
-                    return cloting.idClothing;
+                    clothing.ClothingTag.Add(new ClothingTag { idTag = Int32.Parse(tag), idClothing = clothing.idClothing });
                 }
+                _context.Clothing.Update(clothing);
+                await _context.SaveChangesAsync(cancellationToken);
+                return clothing.idClothing;
             }
         }
     }
